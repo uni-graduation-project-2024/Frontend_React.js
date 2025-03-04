e23b5650af09bd7c5179ab50cd2c7dcdaddee27c
Yasminasoliman <yasmminahatem.400@gmail.com>
2025-03-04 20:25:22 +0200

    admin dashboard & delete user

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -1,19 +1,20 @@
-
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { getAuthToken } from "../../services/auth";
+import linkhost from "../..";
+import "./dashboard.css";
 
 const Dashboard = () => {
   const { token } = getAuthToken();
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
-      .get("https://localhost:7078/api/Admin/view-users", {
+      .get(`${linkhost}/api/Admin/view-users`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -28,9 +29,20 @@ const Dashboard = () => {
       });
   }, []);
 
+  const handleDeleteUser = (userId) =>{
+    axios
+      .delete(`${linkhost}/api/Admin/delete-user/${userId}`)
+      .then(() => {
+        setUsers(users.filter((user) => user.userId !== userId));
+      })
+      .catch((error) => {
+        setError( error.response?.data || "Something went wrong." );
+      });
+  }
+
   return (
-    <div className="container">
-      <h2 className="my-4">Admin Dashboard - User Management</h2>
+    <div className="admin-dashboard-container">
+      <h2 className="admin-table-header">Admin Dashboard - User Management</h2>
       {loading ? (
         <div className="spinner-border" role="status">
           <span className="sr-only">Loading...</span>
@@ -38,7 +50,7 @@ const Dashboard = () => {
       ) : error ? (
         <div className="alert alert-danger">{error}</div>
       ) : (
-        <table className="table table-striped">
+        <table className="admin-table table-striped">
           <thead>
             <tr>
               <th>ID</th>
@@ -49,17 +61,14 @@ const Dashboard = () => {
           </thead>
           <tbody>
             {users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
+              <tr key={user.userId} onClick={() => navigate(`/admin/show-user/${user.userId}`)}>
+                <td>{user.userId}</td>
                 <td>{user.username}</td>
                 <td>{user.email}</td>
                 <td>
-                  <Link to={`/admin/show-user/${user.id}`} className="btn btn-info btn-sm mx-2">
-                    View
-                  </Link>
-                  <Link to={`/admin/update-user/${user.id}`} className="btn btn-warning btn-sm">
-                    Edit
-                  </Link>
+                  <button onClick={() =>handleDeleteUser(user.userId)} className="btn btn-warning btn-sm">
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}

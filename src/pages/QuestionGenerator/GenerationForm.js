fa852f5f9c43cf44e7da94b936921e105a7e2f11
Yasminasoliman <yasmminahatem.400@gmail.com>
2025-04-30 23:42:52 +0300

    genform ui & integrate upload

--- src/pages/QuestionGenerator/GenerationForm.js
+++ src/pages/QuestionGenerator/GenerationForm.js
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; // For navigation
 import "./GenerationForm.css";
 import axios from "axios";
+import linkhost from "../..";
+import { getAuthToken } from "../../services/auth";
 
 const GenerationForm = () => {
   const [file, setFile] = useState(null);
   const [text, setText] = useState("");
-  const [questionType, setQuestionType] = useState([]);
+  const [questionType, setQuestionType] = useState(["MCQ", "True/False"]);
   const [difficulty, setDifficulty] = useState("EASY");
-  const [numQuestions, setNumQuestions] = useState(10);
+  const [numQuestions, setNumQuestions] = useState(13);
   const [examName, setExamName] = useState('');
   const [loading, setLoading] = useState(false);
   const [uploadMode, setUploadMode] = useState("FILE");
+  const [error, setError] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const navigate = useNavigate(); // For navigation
+  const { user } = getAuthToken();
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -36,6 +41,11 @@ const GenerationForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if(!questionType){
+      setError("Please choose the questions type");
+      return;
+    }
+
     const formData = new FormData();
     formData.append("sourceType", uploadMode);
     formData.append("textInput", text);
@@ -43,13 +53,31 @@ const GenerationForm = () => {
     formData.append("difficultyLevel", difficulty);
     formData.append("typeOfQuestions", questionType);
 
+    const formData2 = new FormData();
+    formData2.append('UserId', user.nameid);
+
     if (file) {
+      formData2.append('File', file);  // `file` should be a File object from an input
       formData.append("fileInput", file); // Only append file if it exists
     }
 
     setLoading(true);
 
     try {
+      await axios.get(`${linkhost}/api/Exam/RequestGenerateExam/${user.nameid}`)
+      .catch(() => {
+        setError("You are out of Generation Power");
+        setIsDisabled(true);
+      });
+      await axios.post(`${linkhost}/api/Files/Upload`, formData2, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'accept': '*/*',
+        },
+      }).catch(() => {
+        setError("The file is larger than 5MB or contain more than 50 pages, Please try another file.");
+        setIsDisabled(true);
+      });
       const response = await axios.post(
         "http://localhost:8000/generateQuestions",
         formData,
@@ -62,6 +90,7 @@ const GenerationForm = () => {
 
       // setGeneratedQuestions(response.data); 
       localStorage.setItem("generatedQuestions", JSON.stringify(response.data)); 
+      await axios.put(`${linkhost}/api/Exam/UseOneGenerationPower/${user.nameid}`);
 
       const questionTypeString = questionType.join(" & ")
 
@@ -104,29 +133,33 @@ const GenerationForm = () => {
         </button>
       </div>
 
-      <div className="mb-4">
-  <button  type="button"   onClick={() => {
-      const fileInput = document.createElement("input");
-      fileInput.type = "file";
-      fileInput.accept = ".txt, .pdf, .docx, .pptx";
-      fileInput.onchange = handleFileChange;
-      fileInput.click();
-    }}
-    className="w-full p-2 bg-blue-500 text-white rounded-md">
-    Upload Your File
-  </button>
-</div>
-
-        {uploadMode === "TEXT" && (
-          <div className="mb-4">
-            <textarea rows="4" value={text} onChange={handleTextChange}
-            placeholder="Enter your text" 
-              className="block w-full border rounded-md p-2 bg-gray-700 text-white"/>
-          </div>
-        )}
+      <p>{error}</p>
+        
       <div className="inside-form">
+        <div className="mb-4">
+          {uploadMode === "FILE" && (
+            <>
+            <button  type="button"   onClick={() => {
+                const fileInput = document.createElement("input");
+                fileInput.type = "file";
+                fileInput.accept = ".txt, .pdf, .docx, .pptx";
+                fileInput.onchange = handleFileChange;
+                fileInput.click();
+              }}
+              className="p-2 bg-blue-500 text-white rounded-md">
+              {(file)? "Change" : "  Upload Your File  "}
+            </button>
+            <span style={{marginLeft: "2%"}}>{(file)? file.name : ""}</span>
+            </>
+          )}
+          {uploadMode === "TEXT" && (
+              <textarea rows="4" value={text} onChange={handleTextChange}
+              placeholder="Enter the text to generate from..." 
+                className="block w-full border rounded-md p-2 bg-gray-700 text-white"/>
+          )}
+        </div>
         <div className="mb-44">
-          <h3 className="qg">💡 Type Of Questions</h3>
+          <label className="qg">💡 Type Of Questions</label>
           <div className="flex2 items-center gap-4">
             <label className="flex items-center">
               <input
@@ -151,18 +184,18 @@ const GenerationForm = () => {
           </div>
         </div>
 
-        <div className="mb-4">
-          <h3 className="qg">💡 Difficulty Level</h3>
+        <div className="mb-44">
+          <label className="qg">💡 Difficulty Level</label>
           <div className="flex2 items-center gap-4">
             <label className="flex items-center">
             <input
-  type="radio"
-  name="difficulty"
-  value="EASY"
-  checked={difficulty === "EASY"}
-  onChange={() => setDifficulty("EASY")}
-  className="mr-2"
-/>
+              type="radio"
+              name="difficulty"
+              value="EASY"
+              checked={difficulty === "EASY"}
+              onChange={() => setDifficulty("EASY")}
+              className="mr-2"
+            />
 
               Easy
             </label>
@@ -188,21 +221,21 @@ const GenerationForm = () => {
             </label>
           </div>
         </div> 
-        <div className="mb-4">
+        <div className="mb-44">
           <label className="qg">💡 Number of Questions</label>
           <input
             type="range"
-            min="5"
+            min="6"
             max="20"
             value={numQuestions}
             onChange={(e) => setNumQuestions(e.target.value)}
             className="w-full"
           />
-          <p className="text-center mt-2">{numQuestions}</p>
+          <p className="text-center mt-2" style={{width: "58%"}}>{numQuestions}</p>
         </div>
 
-        <div className="mb-4">
-          <label className="qg1">💡 Title for the Exam</label>
+        <div className="mb-44">
+          <label className="qg">💡 Title for the Exam</label>
           <input
             type="text"
             value={examName}
@@ -215,12 +248,13 @@ const GenerationForm = () => {
 
         <button
           type="submit"
-          className={`w-full py-2 px-4 text-white rounded ${
+          className={`py-2 px-4 text-white rounded ${
             loading
               ? "bg-gray-500 cursor-not-allowed"
               : "bg-blue-500 hover:bg-blue-400"
           }`}
-          disabled={loading}
+          style={{width: "40%"}}
+          disabled={loading || isDisabled}
         >
           {loading ? "Generating..." : "Generate"}
         </button>

3cf9e8a7c46cfbc5b10cbf74abfd0481816ad020
Yasminasoliman <yasmminahatem.400@gmail.com>
2025-05-18 22:33:04 +0300

    sidebar Ui

--- src/Layouts/Sidebar/Sidebar.js
+++ src/Layouts/Sidebar/Sidebar.js
@@ -1,40 +1,41 @@
-import React from 'react';  
+import { NavLink } from 'react-router-dom';
+
 import { FaBook, FaTrophy, FaShoppingCart, FaUser } from 'react-icons/fa';
 import { RiAiGenerate } from "react-icons/ri";
 import { MdLeaderboard } from "react-icons/md";
-import { Link, useNavigate } from 'react-router-dom';
+
+
 import { getAuthToken } from '../../services/auth';
 import './Sidebar.css'; 
 
 const Sidebar = ({ children }) => {
   const { user } = getAuthToken();
-  const navigate = useNavigate();
   
   return (
     <div className="sidebar">
       <div className="logo">
-        <h2>Learntendo</h2>
+        <img src="/images/Learntendo.png" alt="Learntendo Logo"/>
       </div>
       <div className="menu">               
         {user && (
           <>
             {user.role === "User" && (
               <>
-                <Link to="/GenerationForm" className="sidebar-item">
-                  <RiAiGenerate className="icon" style={{ strokeWidth: 1.6 }}/> Generate
-                </Link>
-                <Link to="/library" className="sidebar-item">
-                  <FaBook className="icon" /> My Library
-                </Link>
-                <Link to="/leaderboard" className="sidebar-item">
-                  <MdLeaderboard className="icon" /> Leaderboard
-                </Link>
-                <Link to="/challenges" className="sidebar-item">
-                  <FaTrophy className="icon" /> Challenges
-                </Link>
-                <Link to="/market" className="sidebar-item">
-                  <FaShoppingCart className="icon" /> Market
-                </Link>
+                <NavLink to="/GenerationForm" className={({ isActive }) => isActive ? "active-sidebar sidebar-item" : "sidebar-item"}>
+                  <RiAiGenerate className="icon" style={{ strokeWidth: 1.6 ,fill: "#1226D7", stroke: "#1226D7"}}/> Generate
+                </NavLink>
+                <NavLink to="/library" className={({ isActive }) => isActive ? "active-sidebar sidebar-item" : "sidebar-item"}>
+                  <FaBook className="icon" style={{fill:"#E7494C"}} /> My Library
+                </NavLink>
+                <NavLink to="/leaderboard" className={({ isActive }) => isActive ? "active-sidebar sidebar-item" : "sidebar-item"}>
+                  <MdLeaderboard className="icon" style={{fill:"#E89E00"}}/> Leaderboard
+                </NavLink>
+                <NavLink to="/challenges" className={({ isActive }) => isActive ? "active-sidebar sidebar-item" : "sidebar-item"}>
+                  <FaTrophy className="icon" style={{fill:"#EFDC00"}}/> Challenges
+                </NavLink>
+                <NavLink to="/market" className={({ isActive }) => isActive ? "active-sidebar sidebar-item" : "sidebar-item"}>
+                  <FaShoppingCart className="icon" style={{fill:"#C22AFF"}}/> Market
+                </NavLink>
               </>
             )}
           </>
@@ -42,10 +43,15 @@ const Sidebar = ({ children }) => {
       </div>
 
       {user && (
-        <div className="user-info">
-          <FaUser className="icon" />
-          <div onClick={()=>navigate(`/user-profile/${user.nameid}`)} className="profile-link">{user.unique_name}</div>
-        </div>
+        <NavLink
+          to={`/user-profile/${user.nameid}`}
+          className={({ isActive }) =>
+            isActive ? "user-info active-sidebar" : "user-info"
+          }
+        >
+          <FaUser className="icon" style={{ fill: "#21D7DA" }} />
+          <div className="profile-link">{user.unique_name}</div>
+        </NavLink>
       )}
 
       <div className="sidebar-extra">{children}</div>

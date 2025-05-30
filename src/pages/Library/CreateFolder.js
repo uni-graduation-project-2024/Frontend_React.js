d4b2ae347a5c0fcea7b35995868e1d1d75d311ff
Yasminasoliman <yasmminahatem.400@gmail.com>
2025-05-30 03:37:42 +0300

    create folder Ui with color selection

--- src/pages/Library/CreateFolder.js
+++ src/pages/Library/CreateFolder.js
@@ -9,12 +9,22 @@ const CreateFolder = ({ updateRefresh, onClose }) => {
   const [folderName, setFolderName] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
   const {user} = getAuthToken();
+  const folderColors = {
+    "red": "rgba(229, 77, 77, 0.79)",
+    "blue": "rgba(34, 215, 218, 0.62)",
+    "green": "rgba(63, 222, 90, 0.64)",
+    "yellow": "rgba(239, 244, 100, 0.63)",
+    "purple": "rgba(82, 77, 229, 0.59)",
+    "gray": "rgba(177, 177, 177, 0.63)",
+    "pink": "rgba(255, 70, 184, 0.6)"
+  };
+  const [activeColor, setActiveColor] = useState("red");
 
   const handleFolderSubmit = async () => {
     if (!folderName.trim()) return;
     setIsSubmitting(true);
     
-    const newFolder = { subjectName: folderName };
+    const newFolder = { subjectName: folderName, subjectColor: folderColors[activeColor] };
 
     try {
       const response = await axios.post(linkhost +`/api/Subject/${user.nameid}`, newFolder);
@@ -34,20 +44,40 @@ const CreateFolder = ({ updateRefresh, onClose }) => {
   };
 
   return (
-    <div className="create-folder-container">
-      <h1 className="create-folder-title">Create New Folder</h1>
       <div className="create-folder-box">
         <input
           id="folder-name"
           type="text"
           className="create-folder-input"
-          placeholder='New Folder Name'
+          placeholder='Subject Name'
           value={folderName}
           onChange={(e) => setFolderName(e.target.value)}
           disabled={isSubmitting}
+          maxLength={20}
+          autoFocus
         />
+
+        <div className='subject-colors-container'>
+        {Object.entries(folderColors).map(([colorName, colorValue])=>(
+          <div className="color-picker"
+          key={colorName}
+          onClick={()=> setActiveColor(colorName)}
+          style={{
+            background: colorValue,
+            border: activeColor === colorName ? '3px solid #b8b8b8' : '2px solid transparent',
+          }}>
+            { (activeColor === colorName) &&
+            <svg width="13" height="12" viewBox="0 0 13 12" fill="none" style={{"marginBottom": "2px"}}>
+            <path d="M1 6.14286L5.30435 10L12 1" stroke="#FFFFFF" strokeWidth="3"/>
+            </svg>}
+          </div>
+          ))}
+        </div>
         
         <div className="create-folder-buttons">
+          <button className="create-folder-button"
+          style={{opacity: 0.9}}
+          onClick={onClose}>Cancel</button>
           <button
             onClick={handleFolderSubmit}
             className="create-folder-button"
@@ -55,10 +85,9 @@ const CreateFolder = ({ updateRefresh, onClose }) => {
           >
             {isSubmitting ? 'Creating...' : 'Create'}
           </button>
-          <button className="create-folder-button" onClick={onClose}>Cancel</button>
+
         </div>
       </div>
-    </div>
   );
   
 };

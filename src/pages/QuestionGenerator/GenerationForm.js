13a3f100a0834fa2bb6794db3df01e9a50e92c9a
Yasminasoliman <yasmminahatem.400@gmail.com>
2025-06-05 18:19:40 +0300

    generate form logic update

--- src/pages/QuestionGenerator/GenerationForm.js
+++ src/pages/QuestionGenerator/GenerationForm.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; // For navigation
-import "./GenerationForm.css";
+import { BsLightningFill } from "react-icons/bs";
+import { GrPowerCycle } from "react-icons/gr";
+import { MdOutlineUploadFile } from "react-icons/md";
 import axios from "axios";
+
+import "./GenerationForm.css";
 import linkhost from "../..";
 import { getAuthToken } from "../../services/auth";
 
@@ -46,14 +50,14 @@ const GenerationForm = () => {
       return;
     }
 
-    const formData = new FormData();
+    const formData = new FormData(); //for generate exam endpoint
     formData.append("sourceType", uploadMode);
     formData.append("textInput", text);
     formData.append("numOfQuestions", numQuestions);
     formData.append("difficultyLevel", difficulty);
     formData.append("typeOfQuestions", questionType);
 
-    const formData2 = new FormData();
+    const formData2 = new FormData(); //for upload endpoint
     formData2.append('UserId', user.nameid);
 
     if (file) {
@@ -63,21 +67,32 @@ const GenerationForm = () => {
 
     setLoading(true);
 
-    try {
-      await axios.get(`${linkhost}/api/Exam/RequestGenerateExam/${user.nameid}`)
-      .catch(() => {
+    try{
+      await axios.get(`${linkhost}/api/Exam/RequestGenerateExam/${user.nameid}`);
+    } catch (error) {
         setError("You are out of Generation Power");
         setIsDisabled(true);
-      });
+        setLoading(false);
+        return;
+    }
+    try{
       await axios.post(`${linkhost}/api/Files/Upload`, formData2, {
         headers: {
           'Content-Type': 'multipart/form-data',
           'accept': '*/*',
         },
-      }).catch(() => {
-        setError("The file is larger than 5MB or contain more than 50 pages, Please try another file.");
-        setIsDisabled(true);
       });
+      } catch (error) {
+        const message =
+        error.response?.data?.message || // standard API error structure
+        "Please upload a file (pdf, docx, pptx or txt) not larger than 50MB";
+
+        setError(message);
+        setIsDisabled(true);
+        setLoading(false);
+        return;
+      }
+    try{
       const response = await axios.post(
         "http://localhost:8000/generateQuestions",
         formData,
@@ -117,147 +132,149 @@ const GenerationForm = () => {
     <div className="p-4">
       <form className="md-questionGeneration" onSubmit={handleSubmit}>
         <div className="upload-mode-buttons">
-        <button 
-          type="button" 
-          onClick={() => setUploadMode("FILE")}
-          className={uploadMode === "FILE" ? "active" : "inactive"}
-        >
-          FILE
-        </button>
-        <button 
-          type="button" 
-          onClick={() => setUploadMode("TEXT")}
-          className={uploadMode === "TEXT" ? "active" : "inactive"}
-        >
-          TEXT
-        </button>
-      </div>
+          <button 
+            type="button" 
+            onClick={() => setUploadMode("FILE")}
+            className={uploadMode === "FILE" ? "active" : "inactive"}
+          >
+            FILE
+          </button>
+          <button 
+            type="button" 
+            onClick={() => setUploadMode("TEXT")}
+            className={uploadMode === "TEXT" ? "active" : "inactive"}
+          >
+            TEXT
+          </button>
+        </div>
 
-      <p>{error}</p>
+        <p className="error-message">{error}</p>
         
-      <div className="inside-form">
-        <div className="mb-4">
-          {uploadMode === "FILE" && (
-            <>
-            <button  type="button"   onClick={() => {
-                const fileInput = document.createElement("input");
-                fileInput.type = "file";
-                fileInput.accept = ".txt, .pdf, .docx, .pptx";
-                fileInput.onchange = handleFileChange;
-                fileInput.click();
-                fileInput.required = true;
-              }}
-              className="p-2 bg-blue-500 text-white rounded-md nup-button">
-              {(file)? "Change" : "  Upload Your File  "}
-            </button>
-            <span style={{marginLeft: "2%"}}>{(file)? file.name : ""}</span>
-            </>
-          )}
-          {uploadMode === "TEXT" && (
-              <textarea rows="4" value={text} onChange={handleTextChange}
-              placeholder="Enter the text to generate from..." 
+        <div className="inside-form">
+          <div className="mb-4">
+            {uploadMode === "FILE" && (
+              <>
+              <button  type="button"   onClick={() => {
+                  const fileInput = document.createElement("input");
+                  fileInput.type = "file";
+                  fileInput.accept = ".txt, .pdf, .docx, .pptx";
+                  fileInput.onchange = handleFileChange;
+                  fileInput.click();
+                  fileInput.required = true;
+                }}
+                className="p-2 bg-blue-500 text-white rounded-md nup-button">
+                {(file)? <><GrPowerCycle/> Change </> : <><MdOutlineUploadFile/>  Upload Your File  </>}
+              </button>
+              <span style={{marginLeft: "2%"}}>{(file)? file.name : ""}</span>
+              </>
+            )}
+            {uploadMode === "TEXT" && (
+                <textarea rows="4" value={text} onChange={handleTextChange}
+                placeholder="Enter the text to generate from..." 
                 className="block w-full border rounded-md p-2 bg-gray-700 text-white"/>
-          )}
-        </div>
-        <div className="mb-44">
-          <label className="qg">💡 Type Of Questions</label>
-          <div className="flex2 items-center gap-4">
-            <label className="flex items-center">
-              <input
-                type="checkbox"
-                value="MCQ"
-                checked={questionType.includes("MCQ")}
-                onChange={() => handleQuestionTypeChange("MCQ")}
-                className="mr-2"
-              />
-              MCQ
-            </label>
-            <label className="flex items-center">
-              <input
-                type="checkbox"
-                value="True/False"
-                checked={questionType.includes("True/False")}
-                onChange={() => handleQuestionTypeChange("True/False")}
-                className="mr-2"
-              />
-              True/False
-            </label>
+            )}
           </div>
-        </div>
 
-        <div className="mb-44">
-          <label className="qg">💡 Difficulty Level</label>
-          <div className="flex2 items-center gap-4">
-            <label className="flex items-center">
+          <div className="mb-44">
+            <label className="qg">💡 Type Of Questions</label>
+            <div className="flex2 items-center gap-4">
+              <label className="flex items-center">
+                <input
+                  type="checkbox"
+                  value="MCQ"
+                  checked={questionType.includes("MCQ")}
+                  onChange={() => handleQuestionTypeChange("MCQ")}
+                  className="mr-2"
+                />
+                MCQ
+              </label>
+              <label className="flex items-center">
+                <input
+                  type="checkbox"
+                  value="True/False"
+                  checked={questionType.includes("True/False")}
+                  onChange={() => handleQuestionTypeChange("True/False")}
+                  className="mr-2"
+                />
+                True/False
+              </label>
+            </div>
+          </div>
+
+          <div className="mb-44">
+            <label className="qg">💡 Difficulty Level</label>
+            <div className="flex2 items-center gap-4">
+              <label className="flex items-center">
+              <input
+                type="radio"
+                name="difficulty"
+                value="EASY"
+                checked={difficulty === "EASY"}
+                onChange={() => setDifficulty("EASY")}
+                className="mr-2"
+              />
+
+                Easy
+              </label>
+              <label className="flex items-center">
+                <input
+                  type="radio"
+                  value="MEDIUM"
+                  checked={difficulty === "MEDIUM"}
+                  onChange={(e) => setDifficulty(e.target.value)}
+                  className="mr-2"
+                />
+                Medium
+              </label>
+              <label className="flex items-center">
+                <input
+                  type="radio"
+                  value="HARD"
+                  checked={difficulty === "HARD"}
+                  onChange={(e) => setDifficulty(e.target.value)}
+                  className="mr-2"
+                />
+                Hard
+              </label>
+            </div>
+          </div>
+
+          <div className="mb-44">
+            <label className="qg">💡 Number of Questions</label>
             <input
-              type="radio"
-              name="difficulty"
-              value="EASY"
-              checked={difficulty === "EASY"}
-              onChange={() => setDifficulty("EASY")}
-              className="mr-2"
+              type="range"
+              min="6"
+              max="20"
+              value={numQuestions}
+              onChange={(e) => setNumQuestions(e.target.value)}
+              className="w-full"
             />
-
-              Easy
-            </label>
-            <label className="flex items-center">
-              <input
-                type="radio"
-                value="MEDIUM"
-                checked={difficulty === "MEDIUM"}
-                onChange={(e) => setDifficulty(e.target.value)}
-                className="mr-2"
-              />
-              Medium
-            </label>
-            <label className="flex items-center">
-              <input
-                type="radio"
-                value="HARD"
-                checked={difficulty === "HARD"}
-                onChange={(e) => setDifficulty(e.target.value)}
-                className="mr-2"
-              />
-              Hard
-            </label>
+            <p className="text-center mt-2" style={{width: "58%"}}>{numQuestions}</p>
           </div>
-        </div> 
-        <div className="mb-44">
-          <label className="qg">💡 Number of Questions</label>
-          <input
-            type="range"
-            min="6"
-            max="20"
-            value={numQuestions}
-            onChange={(e) => setNumQuestions(e.target.value)}
-            className="w-full"
-          />
-          <p className="text-center mt-2" style={{width: "58%"}}>{numQuestions}</p>
-        </div>
 
-        <div className="mb-44">
-          <label className="qg">💡 Title for the Exam</label>
-          <input
-            type="text"
-            value={examName}
-            onChange={(e) => setExamName(e.target.value)}
-            className="input-text-title block w-full border rounded-md p-2 bg-gray-700 text-white"
-            required
-          />
-        </div>
-      </div>   
+          <div className="mb-44">
+            <label className="qg">💡 Title for the Exam</label>
+            <input
+              type="text"
+              value={examName}
+              onChange={(e) => setExamName(e.target.value)}
+              className="input-text-title block w-full border rounded-md p-2 bg-gray-700 text-white"
+              required
+            />
+          </div>
+        </div>   
 
         <button
           type="submit"
-          className={`py-2 px-4 text-white rounded ${
+          className={`generate-button py-2 px-4 text-white rounded ${
             loading
               ? "bg-gray-500 cursor-not-allowed"
               : "bg-blue-500 hover:bg-blue-400"
           }`}
-          style={{width: "40%"}}
           disabled={loading || isDisabled}
         >
-          {loading ? "Generating..." : "Generate"}
+          <BsLightningFill className="power-generate" />
+          {loading ? " Generating..." : " Generate"}
         </button>
       </form>
     </div>

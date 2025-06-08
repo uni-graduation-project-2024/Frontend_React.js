206fa37dc99fb1a2e4deee5fd5ef21eb6d388fd0
Yasminasoliman <yasmminahatem.400@gmail.com>
2025-06-08 22:03:49 +0300

    leaderboard css

--- src/pages/Leaderboard/Leaderboard.js
+++ src/pages/Leaderboard/Leaderboard.js
@@ -1,41 +1,59 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import linkhost from "../..";
+import { getAuthToken } from "../../services/auth";
+import { NewbieBadge, BeginnerBadge, ProfessionalBadge, ExpertBadge, MasterBadge } from "../../assets/svg/LeaderboardBadges/LeaderboardBadges";
+
 import "./Leaderboard.css";
 
 const Leaderboard = () => {
   const [users, setUsers] = useState([]);
+  const { user } = getAuthToken();
+
 
   useEffect(() => {
-    setUsers([
-      {
-        id: 1,
-        username: "Amr",
-        xp: 260,
-        level: "Expert",
-        avatar: "https://i.pravatar.cc/40?img=1",
-      },
-      {
-        id: 2,
-        username: "Yasmin",
-        xp: 123,
-        level: "Newbie",
-        avatar: "https://i.pravatar.cc/40?img=2",
-      },
-      {
-        id: 3,
-        username: "Bird",
-        xp: 105,
-        level: "Professional",
-        avatar: "https://i.pravatar.cc/40?img=3",
-      },
-      {
-        id: 4,
-        username: "Cat",
-        xp: 97,
-        level: "Master",
-        avatar: "https://i.pravatar.cc/40?img=4",
-      },
-    ]);
-  }, []);
+    axios.get(`${linkhost}/api/Group?userId=${user.nameid}`)
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching leaderboard data:", error);
+      });
+  }, [user.nameid]);
+
+
+  // useEffect(() => {
+  //   setUsers([
+  //     {
+  //       id: 1,
+  //       username: "Amr",
+  //       xp: 260,
+  //       level: "Expert",
+  //       avatar: "https://i.pravatar.cc/40?img=1",
+  //     },
+  //     {
+  //       id: 2,
+  //       username: "Yasmin",
+  //       xp: 123,
+  //       level: "Newbie",
+  //       avatar: "https://i.pravatar.cc/40?img=2",
+  //     },
+  //     {
+  //       id: 3,
+  //       username: "Bird",
+  //       xp: 105,
+  //       level: "Professional",
+  //       avatar: "https://i.pravatar.cc/40?img=3",
+  //     },
+  //     {
+  //       id: 4,
+  //       username: "Cat",
+  //       xp: 97,
+  //       level: "Master",
+  //       avatar: "https://i.pravatar.cc/40?img=4",
+  //     },
+  //   ]);
+  // }, []);
 
   const getMedal = (index) => {
   if (index === 0) return <span className="medal">🥇</span>;
@@ -51,11 +69,11 @@ const Leaderboard = () => {
   
 
   const levels = [
-    { name: "Newbie", color: "gray", icon: "🛡️" },
-    { name: "Beginner", color: "green", icon: "🛡️" },
-    { name: "Professional", color: "goldenrod", icon: "🛡️" },
-    { name: "Expert", color: "#1e90ff", icon: "🛡️" },
-    { name: "Master", color: "purple", icon: "🛡️" },
+    { name: "Newbie", color: "gray", icon:<NewbieBadge/>},
+    { name: "Beginner", color: "green", icon: <BeginnerBadge/> },
+    { name: "Professional", color: "goldenrod", icon: <ProfessionalBadge/> },
+    { name: "Expert", color: "#1e90ff", icon: <ExpertBadge/> },
+    { name: "Master", color: "purple", icon: <MasterBadge/> },
   ];
 
   return (
@@ -82,11 +100,11 @@ const Leaderboard = () => {
               <li key={user.id} className={`leaderboard-item rank-${index + 1}`}>
                 <div className="leaderboard-rank">
                   <span className="medal">{getMedal(index)}</span>
-                  <img src={user.avatar} alt="avatar" className="avatar" />
+                  <img src={user.avatar || "images/default-profile-avatar.jpg"} alt="pic" className="avatar" />
                   <span className="username">{user.username}</span>
-                  <span className="user-level">({user.level})</span>
+                  <span className="user-level">({levels[user.level].name})</span>
                 </div>
-                <div className="xp">{user.xp} XP</div>
+                <div className="xp">{user.weeklyXp}</div>
               </li>
             ))}
         </ul>

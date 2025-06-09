040c7c7deb777c37d00c1b3df4510b74ae818af2
Yasminasoliman <yasmminahatem.400@gmail.com>
2025-06-09 05:07:32 +0300

    leaderboad integration and ui updates

--- src/pages/Leaderboard/Leaderboard.js
+++ src/pages/Leaderboard/Leaderboard.js
@@ -5,16 +5,21 @@ import { getAuthToken } from "../../services/auth";
 import { NewbieBadge, BeginnerBadge, ProfessionalBadge, ExpertBadge, MasterBadge } from "../../assets/svg/LeaderboardBadges/LeaderboardBadges";
 
 import "./Leaderboard.css";
+import { useNavigate } from "react-router-dom";
+import { TimeUntil } from "../../utils/timeAgo";
 
 const Leaderboard = () => {
   const [users, setUsers] = useState([]);
+  const [ endDate, setEndDate ] = useState(null);
   const { user } = getAuthToken();
+  const navigate = useNavigate();
 
 
   useEffect(() => {
     axios.get(`${linkhost}/api/Group?userId=${user.nameid}`)
       .then(response => {
-        setUsers(response.data);
+        setUsers(response.data.leaderboard);
+        setEndDate(response.data.endDate);
       })
       .catch(error => {
         console.error("Error fetching leaderboard data:", error);
@@ -55,25 +60,22 @@ const Leaderboard = () => {
   //   ]);
   // }, []);
 
-  const getMedal = (index) => {
-  if (index === 0) return <span className="medal">🥇</span>;
-  if (index === 1) return <span className="medal">🥈</span>;
-  if (index === 2) return <span className="medal">🥉</span>;
-  return (
-    <div className="rank-circle">
-      {index + 1}
-    </div>
-  );
-};
+    const getMedal = (index) => {
+    if (index === 0) return <img src="images/first-badge.png" alt="Gold Medal" className="medal" />;
+    if (index === 1) return <div className="rank-circle" style={{backgroundColor:"#bdc1cc", color: "white"}}>{index + 1}</div>;
+    if (index === 2) return <div className="rank-circle" style={{backgroundColor:"#df8c5b", color: "white"}}>{index + 1}</div>;
+    return (
+      <div className="rank-circle">{index + 1}</div>
+    );
+  };
 
-  
 
   const levels = [
-    { name: "Newbie", color: "gray", icon:<NewbieBadge/>},
-    { name: "Beginner", color: "green", icon: <BeginnerBadge/> },
-    { name: "Professional", color: "goldenrod", icon: <ProfessionalBadge/> },
-    { name: "Expert", color: "#1e90ff", icon: <ExpertBadge/> },
-    { name: "Master", color: "purple", icon: <MasterBadge/> },
+    { name: "Newbie", color: "#5f5f5f", icon:<NewbieBadge width={78} height={81}/>, smallIcon: <NewbieBadge width={30} height={33}/> },
+    { name: "Beginner", color: "green", icon: <BeginnerBadge width={72} height={80}/>, smallIcon: <BeginnerBadge width={30} height={38}/> },
+    { name: "Professional", color: "#db9c00", icon: <ProfessionalBadge width={74} height={83}/>, smallIcon: <ProfessionalBadge width={30} height={39}/> },
+    { name: "Expert", color: "rgb(0, 92, 182)", icon: <ExpertBadge width={80} height={82}/>, smallIcon: <ExpertBadge width={30} height={32}/> },
+    { name: "Master", color: "purple", icon: <MasterBadge width={76} height={87}/>, smallIcon: <MasterBadge width={30} height={41}/> },
   ];
 
   return (
@@ -89,24 +91,36 @@ const Leaderboard = () => {
             </div>
           </div>
         ))}
-        <span className="ends-in">Ends In: 3 days</span>
+        <span className="ends-in">Ends <TimeUntil dateString={endDate}/></span>
       </div>
 
       <div className="leaderboard-card">
         <ul>
-          {users
-            .sort((a, b) => b.xp - a.xp)
-            .map((user, index) => (
-              <li key={user.id} className={`leaderboard-item rank-${index + 1}`}>
+          {users.map((user1, index) => {
+          const isCurrentUser = user1.userId === Number(user.nameid);
+          const isFirst = index === 0;
+          const isLast = index === users.length - 1;
+
+          let currentUserClass = "";
+          if (isCurrentUser) {
+            if (isFirst) currentUserClass = "current-user-first";
+            else if (isLast) currentUserClass = "current-user-last";
+            else currentUserClass = "current-user-middle";
+          }
+          return (
+              <li key={user1.userId} className={`leaderboard-item rank-${index + 1} ${currentUserClass}`}
+              >
                 <div className="leaderboard-rank">
                   <span className="medal">{getMedal(index)}</span>
-                  <img src={user.avatar || "images/default-profile-avatar.jpg"} alt="pic" className="avatar" />
-                  <span className="username">{user.username}</span>
-                  <span className="user-level">({levels[user.level].name})</span>
+                  <img src={ user1.profileImage	 || "images/default-profile-avatar.jpg"} alt="pic" className="avatar" />
+                  <span className="username">{user1.username}</span>
+                  <span className="user-level">{levels[user1.level].smallIcon}</span>
                 </div>
-                <div className="xp">{user.weeklyXp}</div>
+                <div className={index === 0 ? "xp xp-first" : (index === users.length-1)? "xp xp-last" : "xp xp-middle"}>
+                  {user1.weeklyXp}</div>
               </li>
-            ))}
+          );
+            })}
         </ul>
       </div>
     </div>
